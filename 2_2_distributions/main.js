7c4f91dc2f848148382e2689817553ea76bdd03c
ellie <ellie.frymire@gmail.com>
2022-10-25 15:09:32 -0700

    week 5

--- 2_2_distributions/main.js
+++ 2_2_distributions/main.js
@@ -1,16 +1,64 @@
 /* CONSTANTS AND GLOBALS */
-// const width = ,
-//   height = ,
-//   margin = ,
-//   radius = ;
+const width = window.innerWidth * 0.7,
+  height = window.innerHeight * 0.7,
+  margin = { top: 20, bottom: 60, left: 60, right: 40 },
+  radius = 5;
+
+let party = "R"
 
 /* LOAD DATA */
-d3.json("[PATH_TO_YOUR_DATA]", d3.autoType)
-  .then(data => {
-    console.log(data)
+d3.json("../data/environmentRatings.json").then(data => {
+  console.log('data', data)
+  const selection = d3.select("#selection")
+  console.log(selection)
 
-    /* SCALES */
-    
-    /* HTML ELEMENTS */
-    
-  });
+  /* SCALES */
+
+  // xScale  - linear,count
+  const xScale = d3.scaleLinear()
+    .domain([0, 1])
+    .range([margin.left, width - margin.right])
+
+  // yScale - linear,count
+  const yScale = d3.scaleLinear()
+    .domain([0, 100])
+    .range([height - margin.bottom, margin.top])
+
+  // colorScale
+  const colorScale = d3.scaleOrdinal()
+    .domain(["R", "D"])
+    .range(["red", "blue"])
+
+  /* HTML ELEMENTS */
+
+  // svg
+  const svg = d3.select("#container")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .style("background-color", "pink")
+  
+  // x axis
+
+  // y axis
+
+  // circles
+  const circles = svg.selectAll(".dot")
+    .data(data)
+    .join(
+      enter => enter
+        .append("circle")
+        .attr("r", 0)
+        .call(sel => sel.transition()
+          .duration(1000)
+          .delay(d => d.ideologyScore2020 * 2000)
+          .attr("r", 10)),
+      update => update,
+      exit => exit,
+    )
+    .attr("class", "dot")
+    .attr("cx", d => xScale(d.ideologyScore2020))
+    .attr("cy", d => yScale(d.envScore2020))
+    .attr("fill", d => colorScale(d.Party))
+
+});
--- 2_3_time_series/main.js
+++ 2_3_time_series/main.js
@@ -1,26 +1,51 @@
  /* CONSTANTS AND GLOBALS */
-// const width = ,
-//   height = ,
-//   margin = ;
+ const width = window.innerWidth * 0.7,
+ height = window.innerHeight * 0.7,
+ margin = { top: 20, bottom: 50, left: 60, right: 60 }
 
 /* LOAD DATA */
-d3.csv('[PATH_TO_YOUR_DATA]', d => {
-  return {
-    // year: new Date(+d.Year, 0, 1),
-    // country: d.Entity,
-    // population: +d.Population
-  }
+d3.csv('../data/populationOverTime.csv', d => {
+ // use custom initializer to reformat the data the way we want it
+ // ref: https://github.com/d3/d3-fetch#dsv
+ return {
+   year: new Date(+d.Year, 0, 1),
+   country: d.Entity,
+   population: +d.Population
+ }
 }).then(data => {
-  console.log('data :>> ', data);
+ console.log('data :>> ', data);
 
-  // SCALES
+//  const usdata = data.filter(d => d.country === "United States")
+//  const chinadata = data.filter(d => d.country === "China")
 
-  // CREATE SVG ELEMENT
+//  console.log('usdata, chinadata', usdata, chinadata)
 
-  // BUILD AND CALL AXES
 
-  // LINE GENERATOR FUNCTION
+ const yScale = d3.scaleLinear()
+  .domain(d3.extent(data, d => d.population))
+  .range([height - margin.bottom, margin.top])
 
-  // DRAW LINE
+const xScale = d3.scaleTime()
+  .domain(d3.extent(data, d => d.year))
+  .range([margin.left, width - margin.right])
 
+const svg = d3.select("#container")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height);
+
+const line = d3.line()
+  .x(d => xScale(d.year))
+  .y(d => yScale(d.population))
+
+const groupedData = d3.groups(data, d => d.country)
+console.log(groupedData)
+
+const path = svg.selectAll("path")
+  .data(groupedData)
+  .join("path")
+  .attr("d", ([country, data]) => line(data))
+  .attr("class", ([country, data]) => country)
+  .attr("stroke", "black")
+  .attr("fill", "none")
 });
